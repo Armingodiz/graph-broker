962823e1ca11980c371a8138501c7cd79b685f8c
Armin <armin.godarzi@utopiops.com>
2022-08-20 21:41:38 +0430

    receiver implemented

--- receiver/main.go
+++ receiver/main.go
@@ -1,31 +1,97 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
-
-	"github.com/zeromq/goczmq"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("receiving...")
-	// Create a router socket and bind it to port 5555.
-	router, err := goczmq.NewRouter("tcp://*:5555")
-	if err != nil {
-		log.Fatal(err)
+	consumerNetwork := os.Getenv("CONSUMER_NETWORK")
+	consumerPort := os.Getenv("CONSUMER_PORT")
+	if consumerNetwork == "" {
+		consumerNetwork = "tcp"
 	}
-	defer router.Destroy()
-
-	log.Println("router created and bound")
-	// Receive the message. Here we call RecvMessage, which
-	// will return the message as a slice of frames ([][]byte).
-	// Since this is a router socket that support async
-	// request / reply, the first frame of the message will
-	// be the routing frame.
-	request, err := router.RecvMessage()
-	if err != nil {
-		log.Fatal(err)
+	if consumerPort == "" {
+		consumerPort = ":81"
 	}
+	listener, err := createListener(consumerNetwork, consumerPort)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	errChan := make(chan error, 1)
+	go func() {
+		for {
+			connection, err := listenningForConnection(listener, consumerPort)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			go handleConnection(connection, errChan)
+		}
+	}()
+	for err := range errChan {
+		panic(err)
+	}
+}
 
-	log.Printf("router received '%s' from '%v'", request[1], request[0])
+func handleConnection(conn net.Conn, errChan chan error) {
+	dataChunc := make([]*Data, 0)
+	for {
+		buffer := make([]byte, 1638400)
+		n, err := conn.Read(buffer[0:])
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				errChan <- err
+			}
+
+		}
+		all := string(buffer[:n])
+		for _, line := range strings.Split(all, "\n") {
+			if line == "" {
+				continue
+			}
+			//fmt.Println(line)
+			var dt Data
+			err = json.Unmarshal([]byte(line), &dt)
+			if err != nil {
+				errChan <- err
+			}
+			dataChunc = append(dataChunc, &dt)
+		}
+	}
+	fmt.Println("received " + strconv.Itoa(len(dataChunc)) + " data")
+}
+
+type Data struct {
+	Round   int    `json:"round"`
+	Sender  int    `json:"sender"`
+	Id      int    `json:"id"`
+	Message string `json:"message"`
+}
+
+func createListener(network, address string) (listener *net.TCPListener, err error) {
+	tcpAddress, err := net.ResolveTCPAddr(network, address)
+	if err != nil {
+		return
+	}
+	listener, err = net.ListenTCP(network, tcpAddress)
+	return
+}
+
+func listenningForConnection(listener *net.TCPListener, port string) (conn net.Conn, err error) {
+	fmt.Println("listenning on port " + port + " ...")
+	conn, err = listener.Accept()
+	if err != nil {
+		return
+	}
+	return
 }
